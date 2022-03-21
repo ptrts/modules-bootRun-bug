b7bfce024da399ef19a8cd0a0afd8283ecbca2e8
Pavel Taruts <ptrtss@gmail.com>
2022-03-21 22:02:26 +0300

    start

---
+++ src/main/java/app/Main.java
@@ -0,0 +1,13 @@
+package app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Module name = " + Main.class.getModule().getName());
+        SpringApplication.run(Main.class, args);
+    }
+}
---
+++ src/main/java/module-info.java
@@ -0,0 +1,7 @@
+module app {
+    requires spring.boot;
+    requires spring.boot.autoconfigure;
+    requires java.annotation;
+    requires spring.context;
+    exports app;
+}
